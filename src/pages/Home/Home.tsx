d1387fa3eca3f4dc904aefabc45fb645fff37a26
Himal Sandaruwan <wahsandaruwan6@gmail.com>
2024-11-20 16:20:59 +0530

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -143,7 +143,7 @@ const Home: React.FC = () => {
           <span 
             className='py-2 font-semibold lg:text-xl' 
             style={{ color: colors.grey[100] }}
-            >User Device Count</span>
+            >Recent Users Device Count</span>
             <AreaChartCom />
         </div>
         <div className="flex flex-col items-center justify-between col-span-1 row-span-6 p-5 rounded-lg lg:col-span-2 md:col-span-2 box">
