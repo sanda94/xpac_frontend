ce52e650abc514e2bb3573514f758ab037c5470b
Himal Sandaruwan <wahsandaruwan6@gmail.com>
2024-11-20 11:04:09 +0530

    Update BaseUrlContext.tsx

--- src/context/BaseUrl/BaseUrlContext.tsx
+++ src/context/BaseUrl/BaseUrlContext.tsx
@@ -8,7 +8,7 @@ interface BaseUrlContextType {
 const BaseUrlContext = createContext<BaseUrlContextType | undefined>(undefined);
 
 export const BaseUrlProvider: React.FC<{children:ReactNode}> = ({children}) => {
-    const [baseUrl , setBaseUrl] = useState<string>("http://localhost:3300/api");
+    const [baseUrl , setBaseUrl] = useState<string>("https://xpacc.online/api");
 
     return (
         <BaseUrlContext.Provider value={{baseUrl , setBaseUrl}}>
